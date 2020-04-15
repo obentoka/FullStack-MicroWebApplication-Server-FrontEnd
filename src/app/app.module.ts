ddfb2c0295ce15ff4f4f315bfdde209a85a21c65
vle <obentoka@gmail.com>
2020-04-15 13:35:58 -0400

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedBackComponent } from './feed-back/feed-back.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedBackComponent,
+    NotFoundComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
